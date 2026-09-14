b5a2a78ac9c91acc5b241f74b74e1cffa2211ae0
No Drama Digital <hello@nodramadigital.com.au>
2026-09-14 20:33:27 +1000

    Update cache version and asset file versions

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-var CACHE = 'wishare-v39';
+var CACHE = 'wishare-v46';
 var SHELL = [
   './',
   'index.php',
   'manifest.json',
-  'assets/css/custom.css?v=39',
-  'assets/js/app.js?v=39',
-  'assets/js/webrtc.js?v=39',
+  'assets/css/custom.css?v=46',
+  'assets/js/app.js?v=46',
+  'assets/js/webrtc.js?v=46',
   'assets/icons/icon-192.png',
   'assets/icons/icon-512.png',
   'assets/audio/incoming.mp3'
